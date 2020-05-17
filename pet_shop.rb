d7918bf569a59b127063b7a1d54b9dd1dcf9034e
Ronan Hughes <rhughes490@gmail.com>
2020-05-17 15:32:35 +0100

    Commit up to Q4

--- pet_shop.rb
+++ pet_shop.rb
@@ -4,3 +4,10 @@ end
 def total_cash(pet_shop)
     return pet_shop[:admin][:total_cash]
 end
+
+def add_or_remove_cash(pet_shop, addition)
+    pet_shop[:admin][:total_cash] += addition
+end
+def total_cash(pet_shop)
+    return pet_shop[:admin][:total_cash]
+end
--- specs/pet_shop_spec.rb
+++ specs/pet_shop_spec.rb
@@ -90,17 +90,17 @@ class TestPetShop < Minitest::Test
      assert_equal(1000, sum)
    end
 
-  # def test_add_or_remove_cash__add
-  #   add_or_remove_cash(@pet_shop,10)
-  #   cash = total_cash(@pet_shop)
-  #   assert_equal(1010, cash)
-  # end
+   def test_add_or_remove_cash__add
+     add_or_remove_cash(@pet_shop,10)
+     cash = total_cash(@pet_shop)
+     assert_equal(1010, cash)
+   end
 
-  # def test_add_or_remove_cash__remove
-  #   add_or_remove_cash(@pet_shop,-10)
-  #   cash = total_cash(@pet_shop)
-  #   assert_equal(990, cash)
-  # end
+   def test_add_or_remove_cash__remove
+     add_or_remove_cash(@pet_shop,-10)
+     cash = total_cash(@pet_shop)
+     assert_equal(990, cash)
+   end
 
   # def test_pets_sold
   #   sold = pets_sold(@pet_shop)
