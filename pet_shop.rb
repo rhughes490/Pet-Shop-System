2695e229b9b9ab91b4a6f4df903498c5dc7f38d6
Ronan Hughes <rhughes490@gmail.com>
2020-05-17 21:04:26 +0100

    Completed up to Q8

--- pet_shop.rb
+++ pet_shop.rb
@@ -16,3 +16,16 @@ end
 def increase_pets_sold(pet_shop, extra_pets)
     return pet_shop[:admin][:pets_sold] += extra_pets
 end
+def stock_count(pet_shop)
+    return pet_shop[:pets].length()
+end
+
+def pets_by_breed(pet_shop, breed)
+    breeds = []
+    for pet in pet_shop[:pets]
+            if pet[:breed] == breed
+            breeds << pet
+            end
+    end
+    return breeds
+end
--- specs/pet_shop_spec.rb
+++ specs/pet_shop_spec.rb
@@ -113,15 +113,15 @@ class TestPetShop < Minitest::Test
      assert_equal(2, sold)
    end
 
-  # def test_stock_count
-  #   count = stock_count(@pet_shop)
-  #   assert_equal(6, count)
-  # end
+   def test_stock_count
+     count = stock_count(@pet_shop)
+     assert_equal(6, count)
+   end
 
-  # def test_all_pets_by_breed__found
-  #   pets = pets_by_breed(@pet_shop, "British Shorthair")
-  #   assert_equal(2, pets.count)
-  # end
+    def test_all_pets_by_breed__found
+      pets = pets_by_breed(@pet_shop, "British Shorthair")
+      assert_equal(2, pets.count)
+    end
 
   # def test_all_pets_by_breed__not_found
   #   pets = pets_by_breed(@pet_shop, "Dalmation")
